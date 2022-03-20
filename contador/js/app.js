eabb30004fa443559ab26d86df3daf817dd6049e
phsdem <pedrohsantosdemoura@gmail.com>
2022-03-20 13:03:50 -0400

    update features

--- contador/js/app.js
+++ contador/js/app.js
@@ -45,7 +45,7 @@ btns.forEach(function(buttons) {
 function onClickDelayEvent (elem) {
    
     function newColor(elem) {
-        elem.style.background = 'red';
+        elem.style.background = 'rgba(255, 48, 0, 0.8)';
         elem.style.color = 'white';
     }
     function normalColor(elem) {
@@ -55,3 +55,58 @@ function onClickDelayEvent (elem) {
     setTimeout(normalColor,3000,elem);
     newColor(elem);
     }
+
+
+function typingEffect() {
+    const contactTexts = shuffleArray(['Counter', 'What Counter?🙃', 'Count Count Count', 'Lets Count🤗', 'Do you need help in count?👍']);
+    const typedtext = document.getElementsByClassName("typed-text")[0];
+    let removing = false;
+    let idx = char = 0;
+
+    setInterval(() => { // We define the interval of the typing speed
+
+        // If we do not reach the limit, we insert characters in the html
+        if (char < contactTexts[idx].length) typedtext.innerHTML += contactTexts[idx][char];
+
+        // 15*150ms = time before starting to remove characters
+        if (char == contactTexts[idx].length + 15) removing = true;
+
+        // Removing characters, the last one always
+        if (removing) typedtext.innerHTML = typedtext.innerHTML.substring(0, typedtext.innerHTML.length - 1);
+
+        char++; // Next character
+
+        // When there is nothing else to remove
+        if (typedtext.innerHTML.length === 0) {
+
+            // If we get to the end of the texts we start over
+            if (idx === contactTexts.length - 1) idx = 0
+            else idx++;
+
+            char = 0; // Start the next text by the first character
+            removing = false; // No more removing characters
+        }
+
+    }, 150); // Typing speed, 150 ms
+
+}
+typingEffect();
+function shuffleArray(array) {
+    let currentIndex = array.length,
+        temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
